757bf13fd6016c164309a1c4e99eea8e22923a86
GameLaxx <tmboisse@gmail.com>
2026-01-16 17:29:43 +0100

    Compute statictics has parameters to launch preprocessing and computing

--- compute_graphs.py
+++ compute_graphs.py
@@ -2,8 +2,8 @@ import pandas as pd
 from plotting import compute_profit_turnover
 
 df = pd.read_csv("statistics.csv", sep=";")
-start_date = "2025-01-01"
-end_date = "2026-01-02"
+start_date = "2025-01-10"
+end_date = "2026-01-11"
 
 print(compute_profit_turnover(df, start_date, end_date))
 
--- compute_statistics.py
+++ compute_statistics.py
@@ -1,29 +1,59 @@
-from paddleocr import PaddleOCR
+import argparse
 import os
 import pandas as pd
-import cv2
+from paddleocr import PaddleOCR
+from preprocessing import process_reset, process_regular
 from exchanges import extract_from_leboncoin, extract_from_vinted, initiate_transaction, write_transaction
 from separation import load_model_platform, sep_platform, sep_buysell
 
-input_dir = "data/raw"
-csv_path = "statistics.csv"
-ocr = PaddleOCR(lang="fr")
-device, clip_model, classifier, preprocess, class_names = load_model_platform("separation/clip_screenshot_classifier.pt")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                    prog='Compute Statistics',
+                    description='This program has 2 main uses : launching the preprocessing for the raw data and computing statistics.',
+                    epilog='Don\'t hesitate to report any issue encountered')
+    parser.add_argument('-r', '--reset', action='store_true', help="When set, reset the folder './data/processed'.")
+    parser.add_argument('-p', '--preprocessing', action='store_true', help="When set, produces preprocessing using the './data/processed' folder.")
+    parser.add_argument('-o', '--output_path', default="statistics.csv", help="Select where will the statistics be stored.")
+    parser.add_argument('-n', '--no_statistics', action='store_true', help="When set, stops before producing statistics. Mainly useful when only preprocessing is wanted.")
+    args = parser.parse_args()
+    assert os.path.isfile(args.output_path), "Select a file that already exist"
 
-initiate_transaction(csv_path)
-with open(csv_path, "a") as f:
-    for file in os.listdir(input_dir):
-        path = os.path.join(input_dir, file)
-        platform = sep_platform(path, device, clip_model, classifier, preprocess, class_names)["label"]
-        tr_type = sep_buysell(path, platform)
-        print(file, platform, tr_type)
-        if platform == "vinted":
-            transactions = extract_from_vinted(ocr, path, tr_type)
-        else:
-            transactions = extract_from_leboncoin(ocr, path, tr_type)
-        write_transaction(f, transactions)
+    input_dir = "data/real"
+    tmp_path = "temporary.csv"
+    annotation_preprocessing_path = "data/metadata/annotations_training.csv"
+    annotation_real_path = "data/metadata/annotations_real.csv"
+    processed_dir = "data/processed"
+    model_path = "separation/clip_screenshot_classifier.pt"
 
-df = pd.read_csv(csv_path, sep=";")
-df["date"] = pd.to_datetime(df["date"],dayfirst=True,format="mixed")
-df.sort_values(by="date", inplace=True, ascending=False)
-df.to_csv(csv_path, sep=";", index=False)
+    if args.reset:
+        process_reset(processed_dir)
+    if args.preprocessing:
+        process_regular(annotation_preprocessing_path, processed_dir)
+    if args.no_statistics:
+        quit()
+
+    ocr = PaddleOCR(lang="fr")
+    device, clip_model, classifier, preprocess, class_names = load_model_platform(model_path)
+    df_annotations = pd.read_csv(annotation_real_path, sep=",")
+
+    initiate_transaction(tmp_path)
+    with open(tmp_path, "a") as f:
+        for file in os.listdir(input_dir):
+            path = os.path.join(input_dir, file)
+            platform = sep_platform(path, device, clip_model, classifier, preprocess, class_names)["label"]
+            tr_type = sep_buysell(path, platform)
+            if platform == "vinted":
+                date = df_annotations[df_annotations["image_id"] == file.split(".")[0]]["date"].values[0]
+                print(file, platform, tr_type, date)
+                transactions = extract_from_vinted(ocr, path, tr_type=tr_type, date=date)
+            else:
+                transactions = extract_from_leboncoin(ocr, path, tr_type)
+            write_transaction(f, transactions)
+    
+    df_tmp = pd.read_csv(tmp_path, sep=";")
+    df_real = pd.read_csv(args.output_path, sep=";")
+    df = pd.concat([df_tmp, df_real])
+    df["date"] = pd.to_datetime(df["date"],dayfirst=True,format="mixed")
+    df = df.sort_values(by="date", ascending=False)
+    df = df.drop_duplicates(subset=["title", "price"])
+    df.to_csv(args.output_path, sep=";", index=False)
